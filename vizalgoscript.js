55921abec387f402df68a10ce6ab5649deb21be7
Akshaya537 <saiprema537@gmail.com>
2026-03-10 23:03:59 +0530

    Add files via upload

---
+++ vizalgoscript.js
@@ -0,0 +1,27 @@
+const toggle = document.createElement("button");
+ toggle.innerText = "🌙";
+ toggle.style.position = "absolute";
+ toggle.style.right = "20px";
+ toggle.style.top = "20px";
+ toggle.style.padding = "8px";
+ toggle.style.cursor = "pointer";
+
+ document.body.appendChild(toggle);
+
+ toggle.addEventListener("click", ()=> {
+ document.body.classList.toggle("dark-mode");
+ if(document.body.classList.contains("dark-mode")){
+    toggle.textContent = "☀️";
+}
+else{
+    toggle.textContent = "🌙";
+}
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+anchor.addEventListener("click",function(e){
+e.preventDefault();
+documentquerySelector(this.getAttribute("href")).scrollIntoView({  behavior:"smooth"
+ })
+ })
+ })
